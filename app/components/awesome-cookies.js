b922323ca1656be64e7f786c745f1751100a4d6f
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-01-18 18:39:33 -0800

    makes a bit more sense

--- app/components/awesome-cookies.js
+++ app/components/awesome-cookies.js
@@ -8,18 +8,9 @@ export default Ember.Component.extend({
   // cm: Ember.inject.service('cookieMonster') 
 
   // bind an attribute to a cached cookie value 
-  isAwesome: Ember.computed.reads('cookieMonster.cookies.isAwesome'),
+  userAvailable: Ember.computed.reads('cookieMonster.cookies.userAvailable'),
   // note, this binding is one way and will not update the cookie 
   // if you change it programatically 
-
-  linkText: Ember.computed('cookieMonster.cookies.ab_link_text', function() {
-    var abVariant = this.get('cookieMonster.cookies.ab_link_text');
-    if (abVariant === 'version1') {
-      return "You won't believe what happens when you click HERE!";
-    } else {
-      return "If you click here THIS will happen to you!";
-    }
-  }),
 
   allCookies: Ember.computed('cookieMonster.cookies', function() {
     return JSON.stringify(this.get('cookieMonster.cookies'));
